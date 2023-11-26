a5ca6774b709176a9a6a855222387fc61c209c11
Rhara Ianna <rharaianna@gmail.com>
2023-11-26 13:50:08 -0300

    corrigi erro not found

--- joaoEMaria.js
+++ joaoEMaria.js
@@ -1,53 +1,47 @@
-
-let joao = 1;
 let maria = 2;
+let joao = 1;
 
-
-function setJoao(valor){
-   if(valor >=0)
-   {
-       joao = valor;
-   }else
-   {
-       joao = 0;
-   }
-
-
-}
-function setMaria(valor)
-{
-   if(valor >= 0)
-   {
-       maria = valor;
-   }else
-   {
-       maria = 0;
-   }
-
-
+function getJoao() {
+  return joao;
 }
 
-function getMaria()
-{
-   return maria;
+function getMaria() {
+  return maria;
 }
-function getJoao()
-{
-   return joao;
-}
-
-
 function deMariaParaJoao()
    {
        joao = joao + maria;
        maria = 0;
    }
 
-   function deJoaoParaMaria()
+function deJoaoParaMaria()
    {
        maria = maria + joao;
        joao = 0
    }
 
+function setJoao(novoValor) {
+  if (novoValor >= 0) {
+    joao = novoValor;
+  } else {
+    joao = 0;
+  }
+}
 
-export {getJoao , getMaria, deJoaoParaMaria , deMariaParaJoao, setJoao, setMaria};
+
+function setMaria(novoValor) {
+  if (novoValor >= 0) {
+    maria = novoValor;
+  } else {
+    maria = 0;
+  }
+}
+
+export {
+  getJoao,
+  getMaria,
+  setJoao,
+  setMaria,
+  deMariaParaJoao,
+  deJoaoParaMaria,
+};
